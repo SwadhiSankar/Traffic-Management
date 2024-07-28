04f8501c40eb542f2d4e3db463f2f3ec70632275
SwadhiSankar <swadhis14@gmail.com>
2024-07-28 21:01:44 +0530

    ViewChild , ContentChild, ContentChildren lifecycles usage

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,5 +1,7 @@
 import {
   Component,
+  ContentChild,
+  contentChildren,
   ElementRef,
   HostBinding,
   HostListener,
@@ -27,8 +29,17 @@ export class ControlComponent {
   // }
   label = input.required<string>();
   private el = inject(ElementRef);
+  // @ContentChild('input') private control?: ElementRef<
+  //   HTMLInputElement | HTMLTextAreaElement
+  // >;
+
+  private control =
+    contentChildren<ElementRef<HTMLInputElement | HTMLTextAreaElement>>(
+      'input'
+    );
   onClick() {
     console.log('clicked');
     console.log(this.el);
+    console.log(this.control());
   }
 }
